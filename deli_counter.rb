d966b75662d691a38080a99770392a77b4ec2d7f
Kristyn Little <kristynlittle@frontier.com>
2021-02-25 05:01:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if line == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |customer|
+  end
+end
+
+def take_a_number(katz_deli, index)
+  katz_deli.each_with_index.map{|customer, index| " Welcome, #{customer}. You are number #{index+1} in line."}
+end
+
+def now_serving(katz_deli)
+  if line >= 1
+    katz_deli.each do
+    puts "Currently serving #{customer}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
+end
