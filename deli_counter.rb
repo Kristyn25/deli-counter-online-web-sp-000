31255360cd3494510f1672cbafd090af1a23da9a
Kristyn Little <kristynlittle@frontier.com>
2021-02-25 07:22:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ end
 
 def take_a_number(katz_deli, customer)
   katz_deli.append(customer)
-  "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
